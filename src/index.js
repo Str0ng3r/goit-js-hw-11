9d4518a276efb23bc655a6da39c147dfeab02b50
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-03-31 03:11:56 +0200

    UPDATE

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 'use strict'
-// import {getResultPhoto} from './searchFunc'
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+import { Notify } from 'notiflix'
 const formSearch = document.querySelector('#search-form')
 const inputSearch = document.querySelector('.input-search')
 const mainGallery = document.querySelector('.main-gallery')
+const photoCard = document.querySelector('.photo-card')
+const buttonLoad = document.querySelector('.load')
 let inputValue = null
 const URLBASIC = 'https://pixabay.com/api/';
 const MYAPI = '34891716-36b65b6efae61fa69d260cb9b'
@@ -12,7 +16,6 @@ inputSearch.addEventListener('input',(event)=> {
     console.log('update')
 })
 
-
 function getResultPhoto (name) {
     axios.get(`${URLBASIC}?key=${MYAPI}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
     .then(res => {
@@ -20,11 +23,20 @@ function getResultPhoto (name) {
         })
         .then(res => {
             const massSearch = res.data.hits;
+            if(massSearch.length > 1){
+                buttonLoad.classList.remove('non-display')
+                buttonLoad.classList.add('have-display')
+                            }
+                            if(massSearch.length === 0 ){
+                            Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+                            }
             console.log(massSearch)
-           const textContents = massSearch.map(el => { return `<div class="photo-card"><img class = 'size-img' src="${el.webformatURL}" alt="${el.tags}" loading="lazy" /><div class="info"><p class="info-item"><b>Likes${el.likes}</b></p><p class="info-item"><b>Views${el.views}</b></p><p class="info-item"><b>Comments${el.comments}</b></p><p class="info-item"><b>Downloads${el.downloads}</b></p></div></div>`;
+           const textContents = massSearch.map(el => { return `<div class="photo-card"><a href='${el.largeImageURL}' class='link-img'><img class = 'size-img' src="${el.webformatURL}" alt="${el.tags}" data-source = '${el.largeImageURL}' loading="lazy" /></a><div class="info"><p class="info-item"><b>Likes</b>${el.likes}</p><p class="info-item"><b>Views</b>${el.views}</p><p class="info-item"><b>Comments</b>${el.comments}</p><p class="info-item"><b>Downloads</b>${el.downloads}</p></div></div>`;
             }).join('')
             console.log(textContents)
             mainGallery.insertAdjacentHTML('beforeend',textContents)
+            let lightbox = new SimpleLightbox('.photo-card a', {captions: true,captionSelector: 'img',captionsType:'text',captionsData:'alt',captionDelay:250});
+            photoCard.next(refresh)
         })
     }
 
