b652b248f263e35cda963c8c1e680e414963f909
Str0ng3r <116167012+Str0ng3r@users.noreply.github.com>
2023-04-01 01:39:37 +0200

    UPDATE

--- src/index.js
+++ src/index.js
@@ -13,43 +13,43 @@ const MYAPI = '34891716-36b65b6efae61fa69d260cb9b'
 import axios from "axios";
 inputSearch.addEventListener('input',(event)=> {
     inputValue = event.target.value.trim()
-    console.log('update')
 })
-
+let page = 0
 function getResultPhoto (name) {
-    axios.get(`${URLBASIC}?key=${MYAPI}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
+    page += 1
+    axios.get(`${URLBASIC}?key=${MYAPI}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
     .then(res => {
          return res
         })
         .then(res => {
             const massSearch = res.data.hits;
+
             if(massSearch.length > 1){
                 buttonLoad.classList.remove('non-display')
                 buttonLoad.classList.add('have-display')
-                            }
-                            if(massSearch.length === 0 ){
-                            Notify.failure('Sorry, there are no images matching your search query. Please try again.')
-                            }
-            console.log(massSearch)
+                }
+                if(massSearch.length === 0 ){
+                Notify.failure('Sorry, there are no images matching your search query. Please try again.')
+                }
+                if(massSearch.length <40){
+                    Notify.info('its last photos')
+                }
            const textContents = massSearch.map(el => { return `<div class="photo-card"><a href='${el.largeImageURL}' class='link-img'><img class = 'size-img' src="${el.webformatURL}" alt="${el.tags}" data-source = '${el.largeImageURL}' loading="lazy" /></a><div class="info"><p class="info-item"><b>Likes</b>${el.likes}</p><p class="info-item"><b>Views</b>${el.views}</p><p class="info-item"><b>Comments</b>${el.comments}</p><p class="info-item"><b>Downloads</b>${el.downloads}</p></div></div>`;
             }).join('')
-            console.log(textContents)
             mainGallery.insertAdjacentHTML('beforeend',textContents)
             let lightbox = new SimpleLightbox('.photo-card a', {captions: true,captionSelector: 'img',captionsType:'text',captionsData:'alt',captionDelay:250});
-            photoCard.next(refresh)
+            lightbox.refresh();
         })
     }
 
-const searchingPhotos = (event) => {
-    event.preventDefault();
+
+
+buttonLoad.addEventListener('click',()=> {
     getResultPhoto(inputValue)
-    }
-
-
-
-
-
-
-
-
-formSearch.addEventListener('submit',searchingPhotos)
+})
+formSearch.addEventListener('submit',(event)=> {
+    event.preventDefault()
+    page = 0
+    mainGallery.innerHTML = '';
+    getResultPhoto(inputValue)
+})
